6448f956fb409aec5fcf45cddb358fb7621c3ac5
JanGolabek <77743690+JanGolabek@users.noreply.github.com>
2022-04-05 12:21:29 +0200

    Fixed UI and implemented a select picture from gallery function. Images shows up in imageView.

--- app/src/main/java/com/example/reversesearcher/MainActivity.kt
+++ app/src/main/java/com/example/reversesearcher/MainActivity.kt
@@ -5,12 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
+import android.widget.ImageView
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var selectButton: Button
+    private lateinit var imageView: ImageView
+
+    companion object {
+        val IMAGE_REQUEST_CODE = 1_000
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        selectButton = findViewById(R.id.selectImageButton)
+        imageView = findViewById(R.id.imageView)
+
+        selectButton.setOnClickListener{
+            pickImageFromGallery()
+        }
 
         val resultsButton = findViewById<Button>(R.id.resultsButton)
         resultsButton.setOnClickListener{
@@ -28,6 +44,18 @@ class MainActivity : AppCompatActivity() {
         selectButton.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+            imageView.setImageURI(data?.data)
         }
     }
 }
